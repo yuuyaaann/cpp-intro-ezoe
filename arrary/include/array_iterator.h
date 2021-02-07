5e73fa9ce9d0c428c8cb9ab3888df59c9daf64b8
yuuyaaann <yuuyaaann@users.noreply.github.com>
2021-02-08 03:58:12 +0900

    Add

--- arrary/include/array_iterator.h
+++ arrary/include/array_iterator.h
@@ -14,11 +14,50 @@ struct array_iterator{
 		return *this;
 	}
 
-	array_iterator & operator --();
+	// 後置
+	array_iterator operator++(int){
+		array_iterator copy = *this;
+		++*this;
+		return copy;
+	}
+
+	// 前置
+	array_iterator & operator --() {
+		--i;
+		return *this;
+	}
+
+
+	// 後値
+	array_iterator & operator --(int) {
+		array_iterator copy = *this;
+		--*this;
+		return copy;
+	}
+
+	array_iterator & operator +=(std::size_t n) {
+		i += n;
+		return *this;
+	}
+
+	array_iterator & operator +(std::size_t n) const {
+			auto copy = *this;
+			copy += n;
+			return copy;
+	}
 
 	// 今参照している要素を返す
 	typename Arrary::const_reference operator *() {
 		return a[i];
+	}
+
+	// 比較
+	bool operator == (array_iterator const & right) {
+		return i == right.i;
+	}
+
+	bool operator != (array_iterator const & right) {
+		return i != right.i;
 	}
 
 };
